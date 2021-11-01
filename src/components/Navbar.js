4096c295d187e2514267884e4c6c8656b0f0f7b2
Kevin Forster <kevin.forster.ch@gmail.com>
2021-11-01 19:38:56 -0400

    Add social links

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,6 @@
 import React from "react"
 import styled from "styled-components"
+import { FaTelegram, FaTwitter } from "react-icons/fa"
 
 import logo from "../assets/images/logo.webp"
 
@@ -7,7 +8,18 @@ const Navbar = () => {
   return (
     <NavContainer id="navbar">
       <nav className="container">
-        <img src={logo} alt="CateMoon Logo" />
+        <img src={logo} alt="CateMoon Logo" loading="lazy" />
+        <div>
+          <a
+            href="https://t.me/joinchat/h-pU65zJ2Pw2OTg0"
+            aria-label="telegram"
+          >
+            <FaTelegram />
+          </a>
+          <a href="https://twitter.com/CateMoonToken" aria-label="twitter">
+            <FaTwitter />
+          </a>
+        </div>
       </nav>
     </NavContainer>
   )
@@ -19,11 +31,30 @@ const NavContainer = styled.header`
 
   nav {
     display: flex;
+    align-items: center;
+    justify-content: space-between;
+
+    & > div {
+      display: flex;
+      align-items: center;
+
+      & > a {
+        font-size: 36px;
+        color: #292255;
+
+        &:not(:last-child) {
+          margin-right: 24px;
+
+          @media (max-width: 425px) {
+            margin-right: 16px;
+          }
+        }
+      }
+    }
   }
 
   img {
     width: 150px;
-    margin: auto;
   }
 `
 
