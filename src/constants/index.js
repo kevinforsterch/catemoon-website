653d327c3573d9d26cfde2994ef24f7c4e8cf772
Kevin Forster <kevin.forster.ch@gmail.com>
2021-12-03 21:59:54 -0500

    Update the website

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,7 +1,9 @@
+// Price Fetch
 export const wBNBAddress = "0xbb4cdb9cbd36b01bd1cbaebf2de08d9173bc095c"
 export const catemoonAddress = "0x1ffeb0b5df9b876d1c3689eedbea392f1652cf8a"
 export const bscScanApiKey = "IMPRTAEKJU79X521EV5ZZDPTWYYKRK8YCI"
 
+// Website Routers
 export const AppRoutes = {
   Home: "/",
   Verify: "/verify",
@@ -9,4 +11,5 @@ export const AppRoutes = {
   NotFound: "*",
 }
 
+// Web3 wallet integration
 export const chainId = "0x38"
